2d9774ec203ade8bb777b8ae53ac62a47ee6fdfa
estudiante <estudiante@192.168.115.5>
2017-02-14 11:23:21 -0500

    Comunicacion server estandar lista

--- HCI_Laberinto/src/server/Comunicacion.java
+++ HCI_Laberinto/src/server/Comunicacion.java
@@ -1,5 +1,117 @@
 package server;
 
-public class Comunicacion{
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Observable;
+
+public class Comunicacion extends Observable implements Runnable {
+
+	private int id;
+
+	private Socket s;
+
+	private boolean conectado;
+
+	public Comunicacion(Socket s, int id) {
+		this.s = s;
+		this.id = id;
+
+		conectado = false;
+	}
+
+	@Override
+	public void run() {
+		// Saludo al cliente
+		enviarMensaje("Hola Cliente");
+
+		while (conectado) {
+			recibirMensaje();
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void recibirMensaje() {
+		DataInputStream dis = null;
+
+		try {
+			dis = new DataInputStream(s.getInputStream());
+
+			String mensaje = dis.readUTF();
+
+			System.out.println("[CONTROL CLIENTE " + id + "]: Se recibio " + mensaje + "del cliente " + id);
+
+			setChanged();
+			notifyObservers(mensaje);
+		} catch (IOException e) {
+			System.err.println("[CONTROL CLIENTE" + id + "] Se perdio coexion con el cliente");
+
+			try {
+				if (dis != null) {
+					dis.close();
+				}
+				s.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+
+			s = null;
+			conectado = false;
+			setChanged();
+			notifyObservers("cliente desconectado");
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void enviarMensaje(String mensaje) {
+		DataOutputStream dos = null;
+
+		try {
+			dos = new DataOutputStream(s.getOutputStream());
+			dos.writeUTF(mensaje);
+			System.out.println("[CONTROL CLIENTE " + id + "]: Se envio mensaje:" + mensaje);
+		} catch (IOException e) {
+			System.err.println("[CONTROL CLIENTE " + id + "]: Se perdio la conexion con el cliente");
+
+			try {
+				if (dos != null) {
+					dos.close();
+				}
+				s.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			s = null;
+			conectado = false;
+			setChanged();
+			notifyObservers("cliente desconectado");
+			e.printStackTrace();
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isConectado() {
+		return conectado;
+	}
+
+	public void setConectado(boolean conectado) {
+		this.conectado = conectado;
+	}
 
 }
--- HCI_Laberinto/src/server/Logica.java
+++ HCI_Laberinto/src/server/Logica.java
@@ -1,24 +1,52 @@
 package server;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Logica implements Observer, Runnable {
 
-	private final int PORT = 5000;
+	private final int PORT = 5666;
 
-	private ServerSocket socketServer;
+	private ServerSocket ss;
 
 	private boolean conectado;
 	private boolean moviendo;
 
 	private ArrayList<Comunicacion> clientes = new ArrayList<>();
 
+	public Logica() {
+
+		try {
+			ss = new ServerSocket(PORT);
+			conectado = true;
+			System.out.println(
+					"[SERVER]: Escuchando en: " + InetAddress.getLocalHost().getHostAddress().toString() + ":" + PORT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public void run() {
 
+		while (conectado) {
+			try {
+				Socket s = ss.accept();
+
+				Comunicacion com = new Comunicacion(s, clientes.size());
+				
+				// agregar el gestor como observador
+				com.addObserver(this);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
